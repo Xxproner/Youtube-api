604379c3a0d7e791081d621fd41d1e9113c7e85c
Xxproner <132216913+Xxproner@users.noreply.github.com>
2024-08-03 20:06:03 +0300

    Update main.go

--- main.go
+++ main.go
@@ -27,25 +27,25 @@ type VideoResourceStruct struct {
 }
 
 type ReplayedMarker struct {
-	startMillis uint64
-	intensityScoreNormalized float32
+	startMillis uint64 // change to StartMillis
+	intensityScoreNormalized float64 // change to IntensityScoreNormalized
 }
 
 /**
  * comp must return true if lhs is less than rhs 
- * clamp similar to c++ clamp but returns -1 if left else positive is right
+ * clamp similar to c++ clamp but returns negative if left and else if right positive
  * */
-func BinarySearchOrNearest(markersList []ReplayedMarker, target ReplayedMarker, startIdx, endIdx int, comp (func(ReplayedMarker, ReplayedMarker) bool), 
-	clamp (func (ReplayedMarker, ReplayedMarker, ReplayedMarker)int)) int {
+func BinarySearchOrNearest(markersList []ReplayedMarker, target *ReplayedMarker, startIdx, endIdx int, comp (func(*ReplayedMarker, *ReplayedMarker) bool), 
+	clamp (func (*ReplayedMarker, *ReplayedMarker, *ReplayedMarker)int)) int {
 	var midIdx int 
 	for startIdx < endIdx {
 		midIdx = (startIdx + endIdx) / 2
 
-		if !comp(markersList[midIdx], target) && !comp(target, markersList[midIdx]) {
+		if !comp(&markersList[midIdx], &target) && !comp(&target, &markersList[midIdx]) {
 			return midIdx
 		}
 
-		if comp(target, markersList[midIdx]) {
+		if comp(&target, &markersList[midIdx]) {
 			endIdx = midIdx - 1
 		} else {
 			startIdx = midIdx + 1
@@ -54,7 +54,7 @@ func BinarySearchOrNearest(markersList []ReplayedMarker, target ReplayedMarker, 
 
 	// find minimum value
 
-	whatIdx := clamp(markersList[startIdx - 1], target, markersList[startIdx])
+	whatIdx := clamp(&markersList[startIdx - 1], &target, &markersList[startIdx])
 
 	if whatIdx < 0 {
 		return startIdx - 1
@@ -63,31 +63,57 @@ func BinarySearchOrNearest(markersList []ReplayedMarker, target ReplayedMarker, 
 	return startIdx // always index of greater value
 }
 
-// func GetMostReplayedSegment(markersList []ReplayedMarker, duration uint64) (int, int, error) {
-// 	if len(markersList) == 0 {
-// 		return -1, -1, errors.New("Empty list")
-// 	}
+/**
+ * return values are first, second idxes and error if any 
+ * */
+func GetMostReplayedSegment(markersList []ReplayedMarker, duration uint64) (int, int, error) {
+	if len(markersList) == 0 {
+		return -1, -1, errors.New("Empty list")
+	}
 
-// 	var mostReplayedValue float32 = 0
-// 	var mostReplayedIdx   int = 0
+	var mostReplayedValue float64 = 0
+	var mostReplayedIdx   int     = 0
 
-// 	// find max intensity in the markers list
-// 	for i := 0; i < len(markersList); i++ {
-// 		if markersList[i].intensityScoreNormalized > mostReplayedValue {
-// 			mostReplayedValue = markersList[i]
-// 			mostReplayedIdx = i
-// 		}
-// 	}
+	// find max intensity in the markers list
+	for i := 0; i < len(markersList); i++ {
+		if markersList[i].intensityScoreNormalized > mostReplayedValue {
+			mostReplayedValue = markersList[i]
+			mostReplayedIdx   = i
+		}
+	}
 
-// 	// check out-of-range 
-// 	if markersList[mostReplayedIdx] + duration / 2 > markersList[len(markersList) - 1] ||
-// 		markersList[mostReplayedIdx] - duration / 2 < 0 {
-// 			return -1, -1, errors.New("Too much duration")
-// 	}
+	// check out-of-range 
+	if markersList[mostReplayedIdx] + duration / 2 > markersList[len(markersList) - 1] ||
+		markersList[mostReplayedIdx] - duration / 2 < 0 {
+			return -1, -1, errors.New("Too long duration")
+	}
 
-// 	var point1Value uint64 = markersList[mostReplayedIdx] - duration / 2;
-// 	var point2Value uint64 = markersList[mostReplayedIdx] + duration / 2;
-// }
+	var point1Value uint64 = markersList[mostReplayedIdx] - duration / 2;
+	var point2Value uint64 = markersList[mostReplayedIdx] + duration / 2;
+
+	var target1 ReplayedMarker = {point1Value}
+	var target2 ReplayedMarker = {point2Value}
+
+	ReplayedMarkerComparer := func(lhs *ReplayedMarker, rhs *ReplayedMarker) bool {
+		return lhs->startMillis < rhs->startMillis
+	}
+
+	// mid guaranteed is between lhs and rhs
+	ReplayedMarkerClamp := func(lhs *ReplayedMarker, mid *ReplayedMarker, rhs *ReplayedMarker) int {
+		if (rhs->startMillis - mid->startMillis < mid->startMillis - lhs->startMillis) {
+			return 1;
+		}
+
+		return -1;
+	}
+
+	var idx1 int = BinarySearchOrNearest(markersList, &target1, 0                  , mostReplayedIdx , ReplayedMarkerComparer, ReplayedMarkerClamp);
+
+	var idx2 int = BinarySearchOrNearest(markersList, &target2, mostReplayedIdx + 1, len(markersList), ReplayedMarkerComparer, ReplayedMarkerClamp);
+
+	_ = idx1; 
+	_ = idx2;
+}
 
 func GetYoutubeVideoId() string{
 	return string("XiCrniLQGYc")
@@ -139,36 +165,11 @@ func main() {
 		// conversion ReplayedMarker from interface{}
 		rawReplayedMarker := rawReplayedMarkersList[i].(map[string]interface{})
 		
-		markersList[i] = ReplayedMarker{uint64(rawReplayedMarker["startMills"].(int)), rawReplayedMarker["intensityScoreNormalized"].(float32)}
+		markersList[i] = ReplayedMarker{uint64(rawReplayedMarker["startMillis"].(float64)), rawReplayedMarker["intensityScoreNormalized"].(float64)}
 	} // get list of replayed markers
 
-	// point1, point2 := GetMostReplayedSegment(replayedMarkersList)
+	point1, point2, getMostReplayedSegmentErr := GetMostReplayedSegment(replayedMarkersList)
+	PanicIfError(getMostReplayedSegmentErr);
 
 	// io.Copy(os.Stdout, resp.Body)
-}
-
-func upper_bound(nums []int, value int, comp func(int, int) bool) uint32 {
-	return upper_bound_Impl(nums, value, uint32(0), uint32(len(nums)), comp)
-}
-
-func upper_bound_Impl(nums []int, value int, startIdx, endInd uint32, comp func(int, int) bool) uint32 {
-	var step uint32 = 0
-	var count uint32 = uint32(len(nums))
-	var it uint32 = 0
-
-	for count > 0 {
-		it = startIdx
-		step = count / 2
-		it += step
-
-		if !comp(value, nums[it]) {
-			it = it + 1
-			startIdx = it
-			count -= step + 1
-		} else {
-			count = step
-		}
-	}
-
-	return it
 }
